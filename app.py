3f5ebf50788d275b1ed2a8ac8f1e5b1d3baecff4
Vitaly Kalinin <xcrsx@icloud.com>
2020-02-04 08:43:19 +0530

    added logger and exceptions handler

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-
+import sys
+import logging
+logging.basicConfig(filename="logs.log",
+                    format='[%(levelname)s]: %(asctime)s [%(module)s > %(funcName)s > %(lineno)d]: %(message)s ',
+                    datefmt='%d.%m.%Y %H:%M:%S', level=logging.INFO)
 from PySide2.QtWidgets import QApplication
 from windscribe_gui.ui import MainWindow
+
+
+logger = logging.getLogger(__name__)
 
 
 class App(MainWindow):
@@ -8,9 +15,12 @@ class App(MainWindow):
 
 
 if __name__ == "__main__":
-    app = QApplication()
-    window = App()
-    window.setFixedSize(400, 450)
-    window.show()
+    try:
+        app = QApplication()
+        window = App()
+        window.setFixedSize(400, 450)
+        window.show()
+    except Exception as error:
+        logger.error(error)
 
     sys.exit(app.exec_())
--- windscribe_gui/ui.py
+++ windscribe_gui/ui.py
@@ -1,10 +1,15 @@
 import os
 import subprocess
 import pexpect
+import logging
 from PySide2 import QtCore, QtWidgets, QtGui, Qt
 from mainwindow import Ui_MainWindow
 from .login_window import Login
 
+
+logger = logging.getLogger(__name__)
+
+common_error = "Error occurred.\nFor more information, view the log file."
 
 US_C = "US Central"
 US = "US East"
@@ -61,30 +66,34 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
        
     def get_account_info(self) -> None:
 
-        self.mainButton.setVisible(True)
-        account = pexpect.spawn("windscribe account")
-        status = account.read()
-        if status == b'Please login to use Windscribe\x1b[0m\r\n':
-            self.informLabel.setText("Please login to use Windscribe")
-            self.logoutButton.setVisible(False)
-            self.mainButton.setText("Log In")
-            self.mainButton.clicked.connect(self.show_login_dialog)
-            self.statusbar.showMessage("")
-        else:
-            self.logoutButton.setVisible(True)
-            status = status.split(b"\r\n")
-            for i in range(len(status)):
-                status[i] = status[i].decode("utf-8").replace("\x1b[0m", "").replace("\x1b[30m", "").replace("\x1b[47m", "")
-            self.informLabel.setText("\n".join(status))
-            self.status = self.get_status()
-            if self.status == "DISCONNECTED":
-                self.mainButton.setText("Connect")
-                self.comboBox.setVisible(True)
-                self.mainButton.clicked.connect(self.connect_to_server)
+        try:
+            self.mainButton.setVisible(True)
+            account = pexpect.spawn("windscribe account")
+            status = account.read()
+            if status == b'Please login to use Windscribe\x1b[0m\r\n':
+                self.informLabel.setText("Please login to use Windscribe")
+                self.logoutButton.setVisible(False)
+                self.mainButton.setText("Log In")
+                self.mainButton.clicked.connect(self.show_login_dialog)
+                self.statusbar.showMessage("")
             else:
-                self.mainButton.setText("Disconnect")
-                self.mainButton.clicked.connect(self.disconnect_server)
-            self.statusbar.showMessage(self.status)
+                self.logoutButton.setVisible(True)
+                status = status.split(b"\r\n")
+                for i in range(len(status)):
+                    status[i] = status[i].decode("utf-8").replace("\x1b[0m", "").replace("\x1b[30m", "").replace("\x1b[47m", "")
+                self.informLabel.setText("\n".join(status))
+                self.status = self.get_status()
+                if self.status == "DISCONNECTED":
+                    self.mainButton.setText("Connect")
+                    self.comboBox.setVisible(True)
+                    self.mainButton.clicked.connect(self.connect_to_server)
+                else:
+                    self.mainButton.setText("Disconnect")
+                    self.mainButton.clicked.connect(self.disconnect_server)
+                self.statusbar.showMessage(self.status)
+        except Exception as error:
+            logger.error(error)
+            self.error.showMessage(common_error)
 
     def show_login_dialog(self) -> None:
 
@@ -95,74 +104,99 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
 
     def log_in(self) -> None:
 
-        self.error.setWindowTitle("Login Error")
-        command = pexpect.spawn("windscribe login")
-        command.expect([pexpect.TIMEOUT ,"Windscribe Username: "])
-        if len(self.log_window.loginLine.text()) >= 3:
-            command.sendline(self.log_window.loginLine.text())
-        else:
-            self.error.showMessage("Username must be at least 3 characters")
-            return
-        command.expect([pexpect.TIMEOUT, "Windscribe Password: "])
-        if len(self.log_window.passwordLine.text()) >= 4:
-            command.sendline(self.log_window.passwordLine.text())
-        else:
-            self.error.showMessage("Password must be at least 4 characters")
-            return
+        try:
+            self.error.setWindowTitle("Login Error")
+            command = pexpect.spawn("windscribe login")
+            command.expect([pexpect.TIMEOUT ,"Windscribe Username: "])
+            if len(self.log_window.loginLine.text()) >= 3:
+                command.sendline(self.log_window.loginLine.text())
+            else:
+                self.error.showMessage("Username must be at least 3 characters")
+                return
+            command.expect([pexpect.TIMEOUT, "Windscribe Password: "])
+            if len(self.log_window.passwordLine.text()) >= 4:
+                command.sendline(self.log_window.passwordLine.text())
+            else:
+                self.error.showMessage("Password must be at least 4 characters")
+                return
 
-        self.log_window.loginLine.clear()
-        self.log_window.passwordLine.clear()
-        answer = command.read()
-        if b"Logged In" in answer:
-            self.mainButton.clicked.disconnect(self.show_login_dialog)
-            self.get_account_info()
-            self.logoutButton.setVisible(True)
-        else:
-            self.error.showMessage("API Error: Could not log in with provided credentials")
-            return
+            self.log_window.loginLine.clear()
+            self.log_window.passwordLine.clear()
+            answer = command.read()
+            if b"Logged In" in answer:
+                self.mainButton.clicked.disconnect(self.show_login_dialog)
+                self.get_account_info()
+                self.logoutButton.setVisible(True)
+            else:
+                self.error.showMessage("API Error: Could not log in with provided credentials")
+                return
+
+        except Exception as error:
+            logger.error(error)
+            self.error.showMessage(common_error)
 
     def get_status(self) -> str:
 
-        message = pexpect.spawn("windscribe status")
-        message_to_show = message.read().split(b"\r\n")
-        for i in range(len(message_to_show)):
-            message_to_show[i] = message_to_show[i].decode("utf-8").replace("\x1b[31m", "").replace("\x1b[0m", "").replace("\x1b[32m", "")
-        if "Service communication error" in message_to_show:
-            self.error.setWindowTitle("Service communication error")
-            self.error.showMessage("Service communication error")
-            return
-        else:
-            return message_to_show[2]
+        try:
+            message = pexpect.spawn("windscribe status")
+            message_to_show = message.read().split(b"\r\n")
+            for i in range(len(message_to_show)):
+                message_to_show[i] = message_to_show[i].decode("utf-8").replace("\x1b[31m", "").replace("\x1b[0m", "").replace("\x1b[32m", "")
+            if "Service communication error" in message_to_show:
+                self.error.setWindowTitle("Service communication error")
+                self.error.showMessage("Service communication error")
+                return
+            else:
+                return message_to_show[2]
+        except IndexError as error:
+            logger.error(error)
+            self.error.showMessage(common_error)
+        except Exception as error:
+            logger.error(error)
+            self.error.showMessage(common_error)
+
+
 
     def connect_to_server(self) -> None:
 
-        if self.comboBox.currentText() == "choose server":
-            command = pexpect.spawn("windscribe connect")
-        else:
-            command = pexpect.spawn(
-                f"windscribe connect {servers[self.comboBox.currentText()]}"
-            )
-        command.expect([pexpect.EOF, "Connected to*"])
-        # 
-        self.mainButton.clicked.disconnect()
-        self.get_account_info()
+        try:
+            if self.comboBox.currentText() == "choose server":
+                command = pexpect.spawn("windscribe connect")
+            else:
+                command = pexpect.spawn(
+                    f"windscribe connect {servers[self.comboBox.currentText()]}"
+                )
+            command.expect([pexpect.EOF, "Connected to*"])
+            self.mainButton.clicked.disconnect()
+            self.get_account_info()
+        except Exception as error:
+            logger.error(error)
+            self.error.showMessage(common_error)
 
     def disconnect_server(self) -> None:
 
-        command = pexpect.spawn("windscribe disconnect")
-        command.expect([pexpect.EOF, "Firewall Disabled*"])
-        self.comboBox.setVisible(False)
-        self.mainButton.clicked.disconnect()
-        self.get_account_info()
+        try:
+            command = pexpect.spawn("windscribe disconnect")
+            command.expect([pexpect.EOF, "Firewall Disabled*"])
+            self.comboBox.setVisible(False)
+            self.mainButton.clicked.disconnect()
+            self.get_account_info()
+        except Exception as error:
+            logger.error(error)
+            self.error.showMessage(common_error)
 
     def log_out(self) -> None:
 
-        self.disconnect_server()
-        command = pexpect.spawn("windscribe logout")
-        answer = command.read()
-        if "Service communication error" in answer.split():
-            self.error.showMessage("Service communication error")
-            return
-        self.comboBox.setVisible(False)
-        self.mainButton.clicked.disconnect()
-        self.get_account_info()
+        try:
+            self.disconnect_server()
+            command = pexpect.spawn("windscribe logout")
+            answer = command.read()
+            if "Service communication error" in answer.split():
+                self.error.showMessage("Service communication error")
+                return
+            self.comboBox.setVisible(False)
+            self.mainButton.clicked.disconnect()
+            self.get_account_info()
+        except Exception as error:
+            logger.error(error)
+            self.error.showMessage(common_error)
